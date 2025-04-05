6a9ffe74c95dfea8d7346b9032c343e57a848274
chinmay2452 <prane702@gmail.com>
2025-04-06 00:57:05 +0530

    setup done

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import { webcrypto } from 'crypto';
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto;
+}
 
 // https://vite.dev/config/
 export default defineConfig({
